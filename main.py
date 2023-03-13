c51021a6e852f3ff4a2f5f2adcdec7fa5bdf910f
Leonardo Koakowski <82957998+3kleo@users.noreply.github.com>
2023-03-12 23:51:54 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import tkinter as tk
 from tkinter import ttk
 import random
+import settings
 
 
 class Minesweeper:
     def __init__(self, dimensions):
+        self.cells = []
         self.game_area = None
         self.top_bar = None
         self.field_visual = None
@@ -23,6 +25,7 @@ class Minesweeper:
         self.print_neighbor_bombs()
         self.print_visual_field()
         self.game_status = True
+        self.create_cells()
         self.initialize_gui()
 
     def generate_field(self):
@@ -31,20 +34,20 @@ class Minesweeper:
                         range(0, (self.dimensions ** 2), self.dimensions)]
 
         self.root = tk.Tk()
-        self.root.geometry('600x700')
+        self.root.geometry(f'{settings.WIDTH}x{settings.HEIGHT}')
         self.root.title("Minesweeper")
         self.root.resizable(False, False)
         self.top_bar = tk.Frame(
             self.root,
             bg='blue',
-            width='600',
+            width=f'{settings.WIDTH}',
             height='100'
         )
         self.top_bar.place(x=0, y=0)
         self.game_area = tk.Frame(
             self.root,
             bg='green',
-            width='600',
+            width=f'{settings.WIDTH}',
             height='600'
         )
         self.game_area.place(x=0, y=100)
@@ -58,7 +61,7 @@ class Minesweeper:
 
         for bomb in bombs:
             self.field[bomb[0]][bomb[1]] = 1
-            
+
     def generate_visual_field(self):
         self.field_visual = [['X' for i in range(self.dimensions)] for j in range(self.dimensions)]
 
@@ -106,7 +109,8 @@ class Minesweeper:
                 neighbors_pos = self.get_neighbor_positions(row, column)
                 # print(neighbors_pos)
                 neighbors = sum(
-                    [(self.field[j[0]][j[1]]) for i, j in enumerate(neighbors_pos) if self.field[j[0]][j[1]] != self.field[row][column]]
+                    [(self.field[j[0]][j[1]]) for i, j in enumerate(neighbors_pos) if
+                     self.field[j[0]][j[1]] != self.field[row][column]]
                 )
                 # neighbors = sorted([(field_l[j[0]][j[1]]) for i, j in enumerate(neighbors_pos) if field_l[j[0]][j[1]] != field_l[row][column]])
                 # print(neighbors)
@@ -124,6 +128,10 @@ class Minesweeper:
                 self.change_visual_field((row, column))
         self.game_status = False
 
+    def is_mine(self, row, column):
+        value = self.field[row][column]
+        return True if value == 1 else False
+
     def check_clicked_space(self, clicked: tuple):
         if clicked not in self.neighbor_clicked:
             self.neighbor_clicked.append(clicked)
@@ -139,7 +147,7 @@ class Minesweeper:
                     row = pos[0]
                     column = pos[1]
 
-                    if self.field[row][column] == 1:
+                    if self.is_mine():
                         print('bomb')
                         self.lose_game()
                     elif self.neighbor_bombs[row][column] > 0:
@@ -147,13 +155,15 @@ class Minesweeper:
                         self.change_visual_field(pos)
                     else:
                         neighbors_pos = self.get_neighbor_positions(row, column)
-                        neighbors_pos = [item for item in neighbors_pos if item != pos and item not in self.neighbors_looped and item not in self.neighbor_clicked]
+                        neighbors_pos = [item for item in neighbors_pos if
+                                         item != pos and item not in self.neighbors_looped and item not in self.neighbor_clicked]
                         # print(neighbors_pos)
                         for neighbor in neighbors_pos:
                             # print(neighbor)
                             # print(neighbor_bombs[neighbor[0]][neighbor[1]])
                             self.neighbor_clicked.append(neighbor)
-                            if self.neighbor_bombs[neighbor[0]][neighbor[1]] == 0 and neighbor not in self.neighbor_to_loop and neighbor not in self.neighbors_looped:
+                            if self.neighbor_bombs[neighbor[0]][neighbor[
+                                1]] == 0 and neighbor not in self.neighbor_to_loop and neighbor not in self.neighbors_looped:
                                 self.neighbor_to_loop.append(neighbor)
 
                     self.neighbor_to_loop = [item for item in self.neighbor_to_loop if item != pos]
@@ -165,23 +175,76 @@ class Minesweeper:
                             self.change_visual_field(pos)
                         break
 
+    def create_cells(self):
+        # pass
+        for row in range(self.dimensions):
+            self.cells.append([])
+            for column in range(self.dimensions):
+                x_pos = 100 + (60 * (column + 1)) 
+                y_pos = 30 + (60 * (row + 1))
+                neighbors = self.neighbor_bombs[row][column]
+                is_mine = self.is_mine()
+                self.cells[row].append(
+                    Slot(self.game_area,
+                         x_pos,
+                         y_pos,
+                         row,
+                         column,
+                         neighbors,
+                         is_mine)
+                )
+
     def initialize_gui(self):
         tk.mainloop()
 
 
 class Slot:
-    def __init__(self, x_pos, y_pos):
+    def __init__(self, place, x_pos, y_pos, row, column, neighbors, is_mine):
+        self.place = place
         self.x_pos = x_pos
         self.y_pos = y_pos
+        self.row = row
+        self.column = column
+        self.create_button()
+        # self.cell = None
+        self.neighbors = neighbors
+        self.is_mine = is_mine
+        self.is_clicked = None
 
     def create_button(self):
-        btn = tk.Button(
-            game_area,
-            bg='red',
-            command=lambda: print_message('abc')
-            )
-        btn.place(x='20', y='20', height='30', width='60')
-        btn.config(text='abcd')
+        self.cell = tk.Frame(self.place,
+                             width=50,
+                             height=50,
+                             background='yellow',
+                             borderwidth=1,
+                             relief="solid")
+        self.cell.place(x=self.x_pos, y=self.y_pos)
+        self.cell.bind("<Button-1>", self.change_color)
+        self.cell.bind("<Button-2>", self.write_on_cell)
+
+    def change_color(self, _event=None):
+        self.cell['bg'] = 'blue'
+
+    def write_on_cell(self, _event=None):
+        var = tk.StringVar()
+        text = tk.Label(self.place,
+                        textvariable=var,
+                        width=1,
+                        height=1,
+                        bg='yellow',
+                        font=("Arial", 15))
+        var.set(f'{self.neighbors}')
+        text_x = self.x_pos + (50 / 3.5)
+        text_y = self.y_pos + (50 / 4)
+        text.place(x=text_x, y=text_y)
+
+        # tk.Button(
+        #     self.place,
+        #     bg='red',
+        #     command=lambda: print_message((self.row, self.column))  # game.check_clicked_space((self.row, self.column))
+        #     )
+        # btn.place(x=str(self.x_pos), y='20', height='30', width='60')
+        # btn.config(text=f'btn {self.printer}')
 
 
 def print_message(msg):
@@ -190,8 +253,9 @@ def print_message(msg):
 
 if __name__ == "__main__":
     game = Minesweeper(5)
-    # root = tk.Tk()
 
+    # root = tk.Tk()
+    #
     # root.geometry('600x700')
     # root.title("Minesweeper")
     # root.resizable(False, False)
@@ -210,14 +274,21 @@ if __name__ == "__main__":
     #     height='600'
     # )
     # game_area.place(x=0, y=100)
+
+    # for i in range(5):
+    #     # btn_text = tk.StringVar(root)
+    #     # btn_text.set(str(i))
+    #     # btn = tk.Button(
+    #     #     game_area,
+    #     #     bg='red',
+    #     #     command=lambda: print_message(btn_text)
+    #     # )
     #
-    # btn = tk.Button(
-    #     game_area,
-    #     bg='red',
-    #     command=lambda: print_message('abc')
-    # )
-    # btn.place(x='20', y='20', height='30', width='60')
-    # btn.config(text='abcd')
+    #     pos_x = (20 + 40) * i
+    #     # pos_y = (20 + 5) * i
+    #     btn = Slot(pos_x, 20, str(i))
+    #     # btn.place(x=str(pos_x), y='20', height='30', width='60')
+    #     # btn.config(text=str(f'btn {i}'))
     # tk.mainloop()
 
     # game = Minesweeper(5)
